f745421eaa40e797f42d3fb31e339a2086ee2eb6
TheComamba <thecomamba@jojoheinze.de>
2024-06-26 20:10:40 +0200

    Making struct serializable.

--- src/generation_parameters.rs
+++ src/generation_parameters.rs
@@ -1,10 +1,12 @@
+use serde::{Deserialize, Serialize};
 use simple_si_units::base::Distance;
 
 /// Parameters for generating a star catalogue.
 /// 
 /// Compare https://github.com/TheComamba/UrsaLumi/blob/main/Documentation/Generation_Algorithm.md#generation-parameters
+#[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct GenerationParameters {
     observer_position: (Distance<f64>, Distance<f64>, Distance<f64>),
-    apparent_magnitude_threshold: f64,
+    apparent_magnitude_limit: f64,
     max_distance: Distance<f64>,
 }
