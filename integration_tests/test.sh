92527fbd04296909876e75519db8814d1e79cc69
TheComamba <thecomamba@jojoheinze.de>
2024-06-26 20:50:39 +0200

    Defining some integration tests.

---
+++ integration_tests/test.sh
@@ -0,0 +1,53 @@
+#/bin/bash
+
+set -e
+
+# Go to git root
+cd $(git rev-parse --show-toplevel)
+
+cargo build --release
+
+exe="./target/release/ursa_lumi"
+
+function expect_success {
+    if [ $? -ne 0 ]; then
+        echo "Test:"
+        echo $1
+        echo "failed."
+        exit 1
+    fi
+}
+
+function expect_failure {
+    if [ $? -eq 0 ]; then
+        echo "Test:"
+        echo $1
+        echo "failed."
+        exit 1
+    fi
+}
+
+function expect_file {
+    if [ ! -f $1 ]; then
+        echo "Test:"
+        echo $2
+        echo "Expected file"
+        echo $1
+        echo "not found."
+        exit 1
+    fi
+}
+
+testname="Running executable without arguments returns an error."
+$exe 2>/dev/null
+expect_failure "$testname"
+
+testname="Running executable with generation parameters but no output fails."
+$exe --params ./integration_tests/example_params.json 2>/dev/null
+expect_failure "$testname"
+
+testname="Running executable with generation parameters and output creates the output."
+rm -rf ./integration_tests/example_output.json
+$exe --params ./integration_tests/example_params.json --out ./integration_tests/example_output.json >/dev/null
+expect_success "$testname"
+expect_file "./integration_tests/example_output.json" "$testname"
--- src/generation_parameters.rs
+++ src/generation_parameters.rs
@@ -1,12 +1,11 @@
 use serde::{Deserialize, Serialize};
-use simple_si_units::base::Distance;
 
 /// Parameters for generating a star catalogue.
 /// 
 /// Compare https://github.com/TheComamba/UrsaLumi/blob/main/Documentation/Generation_Algorithm.md#generation-parameters
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct GenerationParameters {
-    observer_position: (Distance<f64>, Distance<f64>, Distance<f64>),
+    observer_position_in_pc: (f64, f64, f64),
     apparent_magnitude_limit: f64,
-    max_distance: Distance<f64>,
+    max_distance_in_pc: f64,
 }
