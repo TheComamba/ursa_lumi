39fc26251d6ab1acbd6abc320f698edfb5fc8170
TheComamba <thecomamba@jojoheinze.de>
2024-06-30 15:43:47 +0200

    New generation parameter.

--- src/generation_parameters.rs
+++ src/generation_parameters.rs
@@ -8,4 +8,5 @@ pub(crate) struct GenerationParameters {
     observer_position_in_pc: (f64, f64, f64),
     apparent_magnitude_limit: f64,
     max_distance_in_pc: f64,
+    chunksize_in_pc: f64,
 }
