708f1afbbb8f381b9ec1f3984b91d536825c9d16
TheComamba <98019967+TheComamba@users.noreply.github.com>
2024-07-06 09:10:44 +0200

    Chunking the galaxy (#16)

--- Documentation/Generation_Algorithm.md
+++ Documentation/Generation_Algorithm.md
@@ -7,42 +7,44 @@ At the moment, it only supports generating galactic objects.
 ## Generation Parameters
 
 The following parameters need to be specified when starting a simulation:
-- `observer_position`: This describes the `x`, `y` and `z` coordinate of an observer relative to the galactic center.
+- `observer_position_in_pc`: This describes the `x`, `y` and `z` coordinate of an observer relative to the galactic center.
 
     This value cannot be changed in follow-up generations.
 - `apparent_magnitude_limit`: Together with the `observer_position` this restricts the minimal brightness a star must have to be included in the output.
 
     Currently this value can also not be changed in follow-up generations, so choose it wisely.
-- `max_distance`: The maximal distance up to which new stars are generated in this generation run.
+- `max_distance_in_pc`: The maximal distance up to which new stars are generated in this generation run.
+- `chunksize_in_pc`: The length of the edges of chunks in which calculations are processed.
+
+    This value cannot be changed in follow-up generations.
 
 The generation parameters are stored together with the output.
 
 ## Chunking the Galaxy
 
-For the initial generation of stars the local environment of the observer is separated in chunks. Each chunk is a cube with edges that span $S_C = 15 \text{ pc}$. $S_C$ is called the "chunksize". A chunk is therefore characerised by the three ordinates of one of its corners. The first chunk contains the observer at its center. Its corner is therefore at the position
+For the initial generation of stars the local environment of the observer is separated into chunks. Each chunk is a cube with edges that span `chunksize_in_pc` as given by the Generation Parameters. It is denoted $S_C$ here. A chunk is characerised by the three ordinates of one of its corners. The first chunk contains the observer at its center. Its corner is therefore at the position
 $$C_0 = P_{\text{observer}} - \frac{1}{2}(S_C,S_C,S_C) .$$
 
-> The sun is at a distance of `8.2 kpc` from the center of the milky way. Generating stars up to that distance results in
+> The sun is at a distance of $8.2$ kpc from the center of the milky way. Generating stars up to that distance with a chunksize of $S_C=15$ pc results in
 > $$\frac{4}{3} \pi \frac{{8200}^3}{{15}^3} \approx 7\cdot10^8$$
 > chunks.
 
 Due to [branch prediction][branch-prediction], later steps will be faster on the CPU if chunks with similar properties are processed close to each other. Therefore, an algorithm that generates them sorted by their distance is used:
 
 - Calculate `N = floor(max_distance / CHUNKSIZE)`, the number of chunks between the origin and the maximum generation distance along an axis.
-    > To speed up the calculation, allocate the memory for a vector with $\frac{1}{8}\frac{4}{3} \pi N^3$ three-tuples of integers once `N` is known.
-- Fill the first quadrant: For $x\geq0$, $y\geq0$ and $z\geq0$ where
-    $$x\leq N,$$
-    $$y^2 \leq N^2 - x^2,$$
-    $$z^2 \leq N^2 - x^2 - y^2 $$
-    emplace $(x,y,z)$ in the vector.
+- Generate indices for the first octant: For $i_x\geq0$, $i_y\geq0$ and $i_z\geq0$ where
+    $$ i_x\leq N,$$
+    $$ i_x^2 + i_y^2 \leq N^2,$$
+    $$ i_x^2 + i_y^2  + i_z^2 \leq N^2 $$
+    emplace $(i_x,i_y,i_z)$ in the vector.
 - Sort the vector by distance.
 - Create a new vector for all coordinates.
-    > Once again, its size is known beforehand and it can be allocated.
-- Add the zero-chunk containing the `observer_position` at its center.
-- Loop through all entries in the quadrant and all sign combinations $\pm_{x,y,z}$ (in that order, working our way outwards).
+    > Its size is known beforehand and can be allocated.
+- Find the zero-chunk containing the `observer_position` at its center.
+- Loop through all entries in the octant and all sign combinations $\pm_{x,y,z}$ (in that order, working our way outwards).
   - We need to avoid duplicates coming from the fact that `+0 == -0`. Therefore,
-    if `x == -0`, `y == -0` or `z == -0`, continue.`
-  - Add a chunk at $C_0 + (\pm_x x,\pm_y y,\pm_z z) S_C$ to the vector, where $C_0$ is the position of the observer chunk.
+    if `i_x == -0`, `i_y == -0` or `i_z == -0`, continue.`
+  - Add a chunk at $C_0 + (\pm_x i_x,\pm_y i_y,\pm_z i_z) S_C$ to the vector, where $C_0$ is the position of the observer chunk.
 
 ## Adding Stars
 
---
+++ src/galactic_chunk.rs
@@ -0,0 +1,318 @@
+use crate::generation_parameters::GenerationParameters;
+
+/// A chunk of the galaxy.
+#[derive(Debug, Clone)]
+pub(crate) struct GalacticChunk {
+    /// The defining corner of the chunk, in units of parsec.
+    ///
+    /// The chunk is a cube spanning ().
+    pub(crate) corner: (f64, f64, f64),
+}
+
+impl GalacticChunk {
+    /// Separates the galaxy into chunks, up to the maximal generation distance.
+    ///
+    /// Compare https://github.com/TheComamba/UrsaLumi/blob/main/Documentation/Generation_Algorithm.md#chunking-the-galaxy
+    pub(crate) fn generate_chunks(generation_parameters: &GenerationParameters) -> Vec<Self> {
+        let n = number_of_chunks_along_axis(generation_parameters);
+        let chunk_numbers_first_octant = chunk_numbers_in_first_octant(n);
+        Self::create_chunks_in_sphere(generation_parameters, chunk_numbers_first_octant)
+    }
+
+    fn origin_chunk(generation_parameters: &GenerationParameters) -> Self {
+        let mut corner = generation_parameters.observer_position_in_pc;
+        let chunksize = generation_parameters.chunksize_in_pc;
+        corner.0 -= corner.0 % chunksize;
+        corner.1 -= corner.1 % chunksize;
+        corner.2 -= corner.2 % chunksize;
+        Self { corner }
+    }
+
+    fn create_chunks_in_sphere(
+        generation_parameters: &GenerationParameters,
+        chunk_numbers_first_octant: Vec<(usize, usize, usize)>,
+    ) -> Vec<Self> {
+        let origin_chunk = Self::origin_chunk(generation_parameters);
+        let (x0, y0, z0) = origin_chunk.corner;
+
+        let mut chunks: Vec<Self> = Vec::with_capacity(chunk_numbers_first_octant.len() * 8);
+        let size = generation_parameters.chunksize_in_pc;
+        for (x, y, z) in chunk_numbers_first_octant.iter() {
+            for x_sign in [-1, 1].iter() {
+                if x == &0 && x_sign == &-1 {
+                    continue;
+                }
+                let x = *x as f64;
+                let x_sign = *x_sign as f64;
+                for y_sign in [-1, 1].iter() {
+                    if y == &0 && y_sign == &-1 {
+                        continue;
+                    }
+                    let y = *y as f64;
+                    let y_sign = *y_sign as f64;
+                    for z_sign in [-1, 1].iter() {
+                        if z == &0 && z_sign == &-1 {
+                            continue;
+                        }
+                        let z = *z as f64;
+                        let z_sign = *z_sign as f64;
+                        let corner = (
+                            x0 + x * x_sign * size,
+                            y0 + y * y_sign * size,
+                            z0 + z * z_sign * size,
+                        );
+                        chunks.push(GalacticChunk { corner });
+                    }
+                }
+            }
+        }
+        chunks
+    }
+}
+
+#[cfg(test)]
+impl PartialEq for GalacticChunk {
+    fn eq(&self, other: &Self) -> bool {
+        const ACCURACY: f64 = 1e-2;
+        let (x0, y0, z0) = self.corner;
+        let (x1, y1, z1) = other.corner;
+        (x0 - x1).abs() < ACCURACY && (y0 - y1).abs() < ACCURACY && (z0 - z1).abs() < ACCURACY
+    }
+}
+
+#[cfg(test)]
+impl From<(f64, f64, f64)> for GalacticChunk {
+    fn from(corner: (f64, f64, f64)) -> Self {
+        Self { corner }
+    }
+}
+
+#[cfg(test)]
+impl From<&(f64, f64, f64)> for GalacticChunk {
+    fn from(corner: &(f64, f64, f64)) -> Self {
+        Self {
+            corner: corner.clone(),
+        }
+    }
+}
+
+fn chunk_numbers_in_first_octant(n: usize) -> Vec<(usize, usize, usize)> {
+    let mut chunks_first_octant = Vec::new();
+    let radius_squared = (n - 1) * (n - 1);
+    for x in 0..n {
+        for y in 0..n {
+            if y * y + x * x > radius_squared {
+                continue;
+            }
+            for z in 0..n {
+                if x * x + y * y + z * z > radius_squared {
+                    continue;
+                }
+                chunks_first_octant.push((x, y, z));
+            }
+        }
+    }
+    chunks_first_octant.sort_by_key(|&(x, y, z)| x * x + y * y + z * z);
+    chunks_first_octant
+}
+
+fn number_of_chunks_along_axis(generation_parameters: &GenerationParameters) -> usize {
+    (generation_parameters.max_distance_in_pc / generation_parameters.chunksize_in_pc).ceil()
+        as usize
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serial_test::serial;
+
+    #[test]
+    fn test_number_of_chunks_along_axis() {
+        let params = GenerationParameters {
+            observer_position_in_pc: (0., 0., 0.),
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: 5.,
+            chunksize_in_pc: 10.,
+        };
+        assert_eq!(number_of_chunks_along_axis(&params), 1);
+
+        let params = GenerationParameters {
+            observer_position_in_pc: (0., 0., 0.),
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: 55.,
+            chunksize_in_pc: 10.,
+        };
+        assert_eq!(number_of_chunks_along_axis(&params), 6);
+    }
+
+    #[test]
+    fn test_chunk_numbers_in_first_octant() {
+        assert_eq!(chunk_numbers_in_first_octant(1), vec![(0, 0, 0)]);
+        assert_eq!(
+            chunk_numbers_in_first_octant(2),
+            vec![(0, 0, 0), (0, 0, 1), (0, 1, 0), (1, 0, 0)]
+        );
+        assert_eq!(
+            chunk_numbers_in_first_octant(3),
+            vec![
+                (0, 0, 0),
+                (0, 0, 1),
+                (0, 1, 0),
+                (1, 0, 0),
+                (0, 1, 1),
+                (1, 0, 1),
+                (1, 1, 0),
+                (1, 1, 1),
+                (0, 0, 2),
+                (0, 2, 0),
+                (2, 0, 0)
+            ]
+        );
+    }
+
+    #[test]
+    fn chunks_with_small_max_generation_distance_contain_origin_chunk() {
+        let origin = (10., 20., 30.);
+        let params = GenerationParameters {
+            observer_position_in_pc: origin,
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: 0.1,
+            chunksize_in_pc: 10.,
+        };
+        let origin_chunk = GalacticChunk::origin_chunk(&params);
+
+        let chunks = GalacticChunk::generate_chunks(&params);
+
+        assert_eq!(chunks.len(), 1);
+        assert!(chunks[0].eq(&origin_chunk));
+    }
+
+    #[test]
+    fn chunks_do_not_contain_duplications() {
+        for n in 1..10 {
+            let params = GenerationParameters {
+                observer_position_in_pc: (0., 0., 0.),
+                apparent_magnitude_limit: 0.,
+                max_distance_in_pc: 10. * n as f64,
+                chunksize_in_pc: 10.,
+            };
+            let chunks = GalacticChunk::generate_chunks(&params);
+            for (i, chunk) in chunks.iter().enumerate() {
+                for other_chunk in chunks.iter().skip(i + 1) {
+                    assert!(!chunk.eq(other_chunk));
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn chunks_contain_all_expected() {
+        let params = GenerationParameters {
+            observer_position_in_pc: (0.5, 0.5, 0.5),
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: 2.1,
+            chunksize_in_pc: 1.,
+        };
+        let expected_chunks = vec![
+            (0., 0., 0.),
+            (0., 0., 1.),
+            (0., 0., -1.),
+            (0., 1., 0.),
+            (0., -1., 0.),
+            (1., 0., 0.),
+            (-1., 0., 0.),
+            (0., 1., 1.),
+            (0., 1., -1.),
+            (0., -1., 1.),
+            (0., -1., -1.),
+            (1., 0., 1.),
+            (1., 0., -1.),
+            (-1., 0., 1.),
+            (-1., 0., -1.),
+            (1., 1., 0.),
+            (1., -1., 0.),
+            (-1., 1., 0.),
+            (-1., -1., 0.),
+            (1., 1., 1.),
+            (1., 1., -1.),
+            (1., -1., 1.),
+            (1., -1., -1.),
+            (-1., 1., 1.),
+            (-1., 1., -1.),
+            (-1., -1., 1.),
+            (-1., -1., -1.),
+            (0., 0., 2.),
+            (0., 0., -2.),
+            (0., 2., 0.),
+            (0., -2., 0.),
+            (2., 0., 0.),
+            (-2., 0., 0.),
+        ];
+
+        let generated_chunks = GalacticChunk::generate_chunks(&params);
+        for chunk in generated_chunks.iter() {
+            assert!(
+                expected_chunks.contains(&chunk.corner),
+                "{:?} not in {:?}",
+                chunk,
+                expected_chunks
+            );
+        }
+        for chunk in expected_chunks.iter() {
+            assert!(
+                generated_chunks.contains(&chunk.into()),
+                "{:?} not in {:?}",
+                chunk,
+                generated_chunks
+            );
+        }
+    }
+
+    #[test]
+    fn chunks_are_sorted_by_distance() {
+        const ACCURACY: f64 = 1.;
+        let params = GenerationParameters {
+            observer_position_in_pc: (0., 0., 0.),
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: 101.,
+            chunksize_in_pc: 10.,
+        };
+        let (x0, y0, z0) = GalacticChunk::origin_chunk(&params).corner;
+        let chunks = GalacticChunk::generate_chunks(&params);
+        for (i, chunk) in chunks.iter().enumerate() {
+            for other_chunk in chunks.iter().skip(i + 1) {
+                let (x1, y1, z1) = chunk.corner;
+                let (dx1, dy1, dz1) = (x1 - x0, y1 - y0, z1 - z0);
+                let (x2, y2, z2) = other_chunk.corner;
+                let (dx2, dy2, dz2) = (x2 - x0, y2 - y0, z2 - z0);
+                let distance1 = dx1 * dx1 + dy1 * dy1 + dz1 * dz1;
+                let distance2 = dx2 * dx2 + dy2 * dy2 + dz2 * dz2;
+                assert!(distance1 <= distance2 + ACCURACY);
+            }
+        }
+    }
+
+    #[test]
+    #[ignore]
+    #[serial]
+    fn chunc_generation_is_fast() {
+        const TO_MILKY_WAY_CENTER: f64 = 8200.;
+        const MAX_DISTANCE: f64 = TO_MILKY_WAY_CENTER / 3.;
+        let params = GenerationParameters {
+            observer_position_in_pc: (10., 20., 30.),
+            apparent_magnitude_limit: 0.,
+            max_distance_in_pc: MAX_DISTANCE,
+            chunksize_in_pc: 15.,
+        };
+        let start = std::time::Instant::now();
+        let chunks = GalacticChunk::generate_chunks(&params);
+        let duration = start.elapsed();
+        println!(
+            "Generating {} chunks took {:?}, or {:?} per chunk",
+            chunks.len(),
+            duration,
+            duration / chunks.len() as u32
+        );
+        assert!(duration.as_secs_f64() < 1.);
+    }
+}
