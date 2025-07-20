88ebb0d26420a9f07aeb558e83b0c71f6bb836ee
TheComamba <thecomamba@jojoheinze.de>
2025-07-20 20:43:21 +0200

    Replacing from_Gyr with new::<gigayear> - a unit that is not accessible to the crate. Yet. [no ci]

--- src/mass_density/mod.rs
+++ src/mass_density/mod.rs
@@ -1,4 +1,5 @@
-use simple_si_units::base::Time;
+
+use uom::si::f64::Time;
 
 use crate::population::{Population, Subpopulation};
 
@@ -11,20 +12,20 @@ mod outer_spheroid;
 mod outer_thick_disc;
 mod young_thin_disc;
 
-pub(super) fn rho0(population: Population, age: Time<f64>) -> f64 {
+pub(super) fn rho0(population: Population, age: Time) -> f64 {
     match population {
         Population::ThinDisc(Subpopulation::Alive) => {
-            if age < Time::from_Gyr(0.15) {
+            if age < Time::new::<gigayear>(0.15) {
                 4.0e-3
-            } else if age < Time::from_Gyr(1.) {
+            } else if age < Time::new::<gigayear>(1.) {
                 7.9e-3
-            } else if age < Time::from_Gyr(2.) {
+            } else if age < Time::new::<gigayear>(2.) {
                 6.2e-3
-            } else if age < Time::from_Gyr(3.) {
+            } else if age < Time::new::<gigayear>(3.) {
                 4.0e-3
-            } else if age < Time::from_Gyr(5.) {
+            } else if age < Time::new::<gigayear>(5.) {
                 4.0e-3
-            } else if age < Time::from_Gyr(7.) {
+            } else if age < Time::new::<gigayear>(7.) {
                 4.9e-3
             } else {
                 6.6e-3
@@ -41,17 +42,17 @@ pub(super) fn rho0(population: Population, age: Time<f64>) -> f64 {
 pub(super) fn epsilon(population: Population, age: Time<f64>) -> f64 {
     match population {
         Population::ThinDisc(Subpopulation::Alive) => {
-            if age < Time::from_Gyr(0.15) {
+            if age < Time::new::<gigayear>(0.15) {
                 0.0140
-            } else if age < Time::from_Gyr(1.) {
+            } else if age < Time::new::<gigayear>(1.) {
                 0.0268
-            } else if age < Time::from_Gyr(2.) {
+            } else if age < Time::new::<gigayear>(2.) {
                 0.0375
-            } else if age < Time::from_Gyr(3.) {
+            } else if age < Time::new::<gigayear>(3.) {
                 0.0551
-            } else if age < Time::from_Gyr(5.) {
+            } else if age < Time::new::<gigayear>(5.) {
                 0.0696
-            } else if age < Time::from_Gyr(7.) {
+            } else if age < Time::new::<gigayear>(7.) {
                 0.0785
             } else {
                 0.0791
@@ -77,68 +78,68 @@ mod tests {
     fn rho0_is_correct() {
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.1),
+            Time::new::<gigayear>(0.1),
         );
         assert_ratio!(4.0e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.5),
+            Time::new::<gigayear>(0.5),
         );
         assert_ratio!(7.9e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(1.5),
+            Time::new::<gigayear>(1.5),
         );
         assert_ratio!(6.2e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(2.5),
+            Time::new::<gigayear>(2.5),
         );
         assert_ratio!(4.0e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(3.5),
+            Time::new::<gigayear>(3.5),
         );
         assert_ratio!(4.0e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(5.5),
+            Time::new::<gigayear>(5.5),
         );
         assert_ratio!(4.9e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(7.5),
+            Time::new::<gigayear>(7.5),
         );
         assert_ratio!(6.6e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThinDisc(Subpopulation::WhiteDwarf),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_ratio!(3.96e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThickDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_ratio!(1.34e-3, rho, ACC);
 
         let rho = rho0(
             Population::ThickDisc(Subpopulation::WhiteDwarf),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_ratio!(3.04e-4, rho, ACC);
 
-        let rho = rho0(Population::Spheroid, Time::from_Gyr(0.));
+        let rho = rho0(Population::Spheroid, Time::new::<gigayear>(0.));
         assert_ratio!(9.32e-6, rho, ACC);
 
-        let rho = rho0(Population::Bulge, Time::from_Gyr(0.));
+        let rho = rho0(Population::Bulge, Time::new::<gigayear>(0.));
         assert_diff!(0., rho, ACC);
     }
 
@@ -146,68 +147,68 @@ mod tests {
     fn epsilon_is_correct() {
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.1),
+            Time::new::<gigayear>(0.1),
         );
         assert_ratio!(0.0140, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.5),
+            Time::new::<gigayear>(0.5),
         );
         assert_ratio!(0.0268, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(1.5),
+            Time::new::<gigayear>(1.5),
         );
         assert_ratio!(0.0375, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(2.5),
+            Time::new::<gigayear>(2.5),
         );
         assert_ratio!(0.0551, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(3.5),
+            Time::new::<gigayear>(3.5),
         );
         assert_ratio!(0.0696, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(5.5),
+            Time::new::<gigayear>(5.5),
         );
         assert_ratio!(0.0785, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::Alive),
-            Time::from_Gyr(7.5),
+            Time::new::<gigayear>(7.5),
         );
         assert_ratio!(0.0791, eps, ACC);
 
         let eps = epsilon(
             Population::ThinDisc(Subpopulation::WhiteDwarf),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_diff!(0., eps, ACC);
 
         let eps = epsilon(
             Population::ThickDisc(Subpopulation::Alive),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_diff!(0., eps, ACC);
 
         let eps = epsilon(
             Population::ThickDisc(Subpopulation::WhiteDwarf),
-            Time::from_Gyr(0.),
+            Time::new::<gigayear>(0.),
         );
         assert_diff!(0., eps, ACC);
 
-        let eps = epsilon(Population::Spheroid, Time::from_Gyr(0.));
+        let eps = epsilon(Population::Spheroid, Time::new::<gigayear>(0.));
         assert_ratio!(0.76, eps, ACC);
 
-        let eps = epsilon(Population::Bulge, Time::from_Gyr(0.));
+        let eps = epsilon(Population::Bulge, Time::new::<gigayear>(0.));
         assert_diff!(0., eps, ACC);
     }
 }
